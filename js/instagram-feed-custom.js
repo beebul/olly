d097f8a8c8b697a153e4f75fd9d4b770facf49ff
David McGarry <beebul@gmail.com>
2017-11-10 14:14:40 +1030

    lazy loading

--- js/instagram-feed-custom.js
+++ js/instagram-feed-custom.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
         resolution: 'standard_resolution',
         accessToken: '47217023.3d3c6ba.5110c05ce10f4cecaa98595564fae9fd',
         sortBy: 'most-recent',
-        template: '<div class="4u 6u(medium) 6u(small) 12u(xsmall) image-fit instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        template: '<div class="4u 6u(medium) 6u(small) 12u(xsmall) image-fit instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',        
+        //template: '<div class="4u 6u(medium) 6u(small) 12u(xsmall) image-fit instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a><h2><span>{{caption}}</span></h2></div>',
     });
     userFeed.run();
     
@@ -18,6 +19,8 @@ $(document).ready(function() {
     $('.gallery').magnificPopup({
         type: 'image',
         delegate: 'a',
+        preload: [0,2], // read about this option in next Lazy-loading section
+        navigateByImgClick: true,
         gallery: {
             enabled: true
         }
