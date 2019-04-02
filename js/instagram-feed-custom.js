db7a6e7d0d1b0dd7889bfa788a2ee7af7fd9931e
David McGarry <beebul@gmail.com>
2019-04-02 15:36:38 +1030

    Update instagram-feed-custom.js

--- js/instagram-feed-custom.js
+++ js/instagram-feed-custom.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
         userId: 47217023,
         limit: 12,
         resolution: 'standard_resolution',
-        accessToken: '47217023.3d3c6ba.5110c05ce10f4cecaa98595564fae9fd',
+        /////// accessToken: '47217023.3d3c6ba.5110c05ce10f4cecaa98595564fae9fd',
+        accessToken: '47217023.ba4c844.d10e45cf32fe468b8274e17bf6798f51',
         sortBy: 'most-recent',
         template: '<div class="instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" height="320" class="img-fluid"/></a></div>',
     });
